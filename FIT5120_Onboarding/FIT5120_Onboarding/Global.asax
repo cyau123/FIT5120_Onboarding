109b3d2fbd36dcc79991c618ebaea7d43aa90930
cyau123 <102947465+cyau123@users.noreply.github.com>
2023-03-09 15:21:18 +1100

    first push

---
+++ FIT5120_Onboarding/FIT5120_Onboarding/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5120_Onboarding.MvcApplication" Language="C#" %>
---
+++ FIT5120_Onboarding/FIT5120_Onboarding/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5120_Onboarding.Startup))]
+namespace FIT5120_Onboarding
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
