c3c4058d60d7873872c62f986fce8f1f0cc59c6d
cyau123 <102947465+cyau123@users.noreply.github.com>
2023-03-10 01:44:29 +1100

    complete function - get next collection date

---
+++ FIT5120_Onboarding/FIT5120_Onboarding/Models/Schedule.cs
@@ -0,0 +1,28 @@
+namespace FIT5120_Onboarding.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Schedule
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string CollectionDay { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime NextWaste { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime NextRecycle { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime NextGreen { get; set; }
+    }
+}
