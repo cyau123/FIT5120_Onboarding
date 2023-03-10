95f577efe73dc7d4c13591323ad95df8ab16de8f
Eduardo Trujillo <act.jetv@gmail.com>
2023-03-11 01:56:03 +1100

    CLEANED USER STORIES DATA

--- utils.py
+++ utils.py
@@ -2,6 +2,20 @@ import pandas as pd
 import pyodbc
 import sqlalchemy
 
+def df_userstory1(path):
+    df = pd.read_excel(path)
+    df = df.rename(columns={"Unnamed: 0":"TYPE"})
+    df.columns = df.columns.str.upper()
+    df.iloc[:, 0] = df.iloc[:, 0].fillna(method="ffill")
+
+    for col in df.columns[1:]:
+        df[col] = df[col].str.replace(r"[^a-zA-Z0-9\s]+", "")
+        df[col] = df[col].str.upper()
+
+    df.iloc[:,-1] = df.iloc[:,-1].str.cat(["."]*len(df), sep = "")
+    
+    return df
+
 def df_userstory2(path):
     df = pd.read_csv(path, sep=";")
 
@@ -35,12 +49,16 @@ def create_table(server,
     
     print("Pushed table")
 
-create_table(server = 'ta5.cs9naymhw4no.ap-southeast-2.rds.amazonaws.com;',
+list(map(lambda df:
+    create_table(server = 'ta5.cs9naymhw4no.ap-southeast-2.rds.amazonaws.com;',
              database = 'ONBOARDING',
              user = 'fit5120ta5',
              pwd = 'ta5workmail123',
-             df = df_userstory2("us_2_2.csv"),
-             name_table = 'US_2_2')
+             df = df,
+             name_table = 'US_2_1'),
+    [df_userstory1("us_2_1.xlsx"), df_userstory2("us_2_2.csv")]
+    ))
+
 
 # testing ----
 
@@ -56,6 +74,7 @@ df = pd.read_sql("SELECT * FROM US_2_2", sql_connection)
 sql_connection.close()
 # df.dtypes
 # df.to_csv("kk.csv")
+
 
 
 
