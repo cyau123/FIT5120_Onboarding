e928b0a7146da11c81d122ce6d3f61deca46663d
cyau123 <102947465+cyau123@users.noreply.github.com>
2023-03-13 12:20:22 +1100

    - remove garbageQuery controller and view, moved to wasteManagementGuide controller and view. Add redirect actions

--- FIT5120_Onboarding/FIT5120_Onboarding/Controllers/WasteManagementGuideController.cs
+++ FIT5120_Onboarding/FIT5120_Onboarding/Controllers/WasteManagementGuideController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FIT5120_Onboarding.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace FIT5120_Onboarding.Controllers
 {
     public class WasteManagementGuideController : Controller
     {
+        private FIT5120_Models db = new FIT5120_Models();
+
         // GET: WasteManagementGuide
         public ActionResult Index()
         {
@@ -27,6 +30,40 @@ namespace FIT5120_Onboarding.Controllers
         public ActionResult YellowBin()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SearchBin(GarbageQuery model)
+        {
+            if (ModelState.IsValid)
+            {
+                string Name = model.GarbageEntered;
+                var garbage = db.Garbages
+                              .Where(s => s.GarbageName == Name)
+                              .FirstOrDefault<Garbage>();
+
+                if (garbage != null)
+                {
+                    model.BinColour = garbage.Color;
+                    if (model.BinColour.ToUpper().Equals("YELLOW"))
+                    {
+                        return RedirectToAction("YellowBin");
+                    }
+                    else if (model.BinColour.ToUpper().Equals("GREEN"))
+                    {
+                        return RedirectToAction("GreenBin");
+                    }
+                    else
+                    {
+                        return RedirectToAction("RedBin");
+                    }
+                }
+                else
+                {
+                    model.BinColour = "Type of garbage cannot be found";
+                }
+            }
+            return View("Index", model);
         }
     }
 }
